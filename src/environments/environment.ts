c3c6529ed39217bbc0ba32b6eebd27dc9347010a
chanchalagnihotri87 <74895265+chanchalagnihotri87@users.noreply.github.com>
2025-10-25 12:02:47 +0530

    Update buyerPortalUrl in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'https://vein360-api-dev-auekcfcdfkdggace.canadacentral-01.azurewebsites.net',
-  buyerPortalUrl: 'https://nice-ocean-06b8a210f.1.azurestaticapps.net',
+  buyerPortalUrl: 'https://mango-plant-03bf20c0f.3.azurestaticapps.net',
 };
